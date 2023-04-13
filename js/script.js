2546d9c3e2d7d3e0a5959806ca7c9378f4507a87
Agnieszka Dawidowicz <agnieszka.dawidowicz.com>
2023-04-13 12:29:35 +0200

    Added active to clickedElement

--- js/script.js
+++ js/script.js
@@ -6,14 +6,27 @@
 */
 
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
+   
+   /* [DONE] remove class 'active'from all article links */
+   const activeLinks = document.querySelectorAll('.titles a.active');
 
-    /* remove class 'active' from all article links  */
+   for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+   }
 
-    /* add class 'active' to the clicked link */
+   /* [IN PROGRESS] add class 'active' to the clicked link */
+   console.log('clickedElement:', clickedElement);
+   clickedElement.classList.add('active');
 
-    /* remove class 'active' from all articles */
+   /* [DONE] remove class 'active' from all articles */
+
+   const activeArticles = document.querySelectorAll('.posts article.active');
+
+   for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+   }
 
     /* get 'href' attribute from the clicked link */
 
