2934cc29e7a59bf4d9c522fd748e16eb97533f3e
Agnieszka Dawidowicz <agnieszka.dawidowicz.com>
2023-04-14 10:36:08 +0200

    Remove class active from articles, get 'href' attribute and add class

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 'use strict';
-/*document.getElementById('test-button').addEventListener('click', function(){
+/*
+    document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
   });
 */
 
-const titleClickHandler = function(event){
+    const titleClickHandler = function(event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
    
    /* [DONE] remove class 'active'from all article links */
    const activeLinks = document.querySelectorAll('.titles a.active');
-
    for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
    }
 
-   /* [IN PROGRESS] add class 'active' to the clicked link */
+   /* [DONE] add class 'active' to the clicked link */
    console.log('clickedElement:', clickedElement);
    clickedElement.classList.add('active');
 
@@ -28,15 +29,20 @@ const titleClickHandler = function(event){
     activeArticle.classList.remove('active');
    }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
 
     /* find the correct article using the selector (value of 'href' attribute) */
 
-    /* add class 'active' to the correct article */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
   }
 
-  const links = document.querySelectorAll('.titles a');
+    const links = document.querySelectorAll('.titles a');
 
-  for(let link of links){
+    for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
